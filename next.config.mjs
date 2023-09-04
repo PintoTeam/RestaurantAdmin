061e656f8f7b9021757cffa530ad3eb10fd074a9
osanchez25 <osanchez25@hotmail.com>
2023-09-04 15:03:43 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,21 @@
 /**
-* @type {import('next').NextConfig}
-*/
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   images: {
     loader: 'akamai',
     path: '',
   },
   assetPrefix: './',
+  target: 'serverless', // Add this line to set the target to 'serverless'
+  exportPathMap: async function () {
+    return {
+      '/': { page: '/' },
+      // Add other routes as needed
+    };
+  },
+  // Use the "output: 'export'" option
+  output: 'export',
 };
 
 export default nextConfig;
