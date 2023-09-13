23ab85de90dcbc9fdec82cbbf0a9b671d82c6efb
Fabian Fernandez <fdzfabian@gmail.com>
2023-09-13 11:12:02 -0600

    Missing comma

--- src/app/(with-menu)/(protected)/home/page.tsx
+++ src/app/(with-menu)/(protected)/home/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   const { user } = useStore();
   return (
     <main className="w-full min-h-full text-left flex flex-col pt-5 font-semibold">
-      <h1 className="m-auto text-5xl">Hello {user?.name}!</h1>
+      <h1 className="m-auto text-5xl">Hello, {user?.name}!</h1>
     </main>
   );
 }
