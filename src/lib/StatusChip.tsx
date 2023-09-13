7247acb3285f58079fc5a0b62868c629df5fb363
Fabian Fernandez <fdzfabian@gmail.com>
2023-09-13 03:38:19 -0600

    Fix status chips color

--- src/lib/StatusChip.tsx
+++ src/lib/StatusChip.tsx
@@ -8,19 +8,19 @@ export function StatusChip({ active, disabled }: StatusChipProps) {
     <div
       className={`flex flex-row items-center px-2 py-1 rounded-full text-sm w-fit justify-start space-x-2 font-semibold ${
         !disabled && active
-          ? "bg-green-100 text-green-800 border-solid border-[1px] border-green-800"
+          ? "bg-green-100 text-green-600 border-solid border-[1px] border-green-600"
           : !disabled && !active
-          ? "bg-red-100 text-red-800 border-solid border-[1px] border-red-800"
-          : "bg-gray-100 text-gray-800 border-solid border-[1px] border-gray-800"
+          ? "bg-red-100 text-red-600 border-solid border-[1px] border-red-600"
+          : "bg-gray-100 text-gray-600 border-solid border-[1px] border-gray-600"
       }`}
     >
       <div
         className={`w-2 h-2 rounded-full ${
           !disabled && active
-            ? "bg-green-800"
+            ? "bg-green-600"
             : !disabled && !active
-            ? "bg-red-800"
-            : "bg-gray-800"
+            ? "bg-red-600"
+            : "bg-gray-600"
         }`}
       />
       <span>{active ? "Activo" : "Inactivo"}</span>
