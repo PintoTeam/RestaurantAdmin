01b97bde6994034ea532acc0f2ccd9f957fdca2c
Fabian Fernandez <fdzfabian@gmail.com>
2023-09-13 13:06:48 -0600

    Add new layout (more responsive)

--- src/app/(with-menu)/layout.tsx
+++ src/app/(with-menu)/layout.tsx
@@ -7,10 +7,12 @@ export default function RootLayout({
 }) {
   return (
     <>
-      <div className="basis-2/12">
+      <div className="basis-1/12">
         <Sidebar />
       </div>
-      <section className="basis-10/12 py-5 px-8 min-h-full">{children}</section>
+      <section className="basis-11/12 py-5 px-8 min-h-full overflow-auto">
+        {children}
+      </section>
     </>
   );
 }
