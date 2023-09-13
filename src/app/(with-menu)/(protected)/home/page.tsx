e13d224efed27a37d716552e7da84cd06a89eac4
Fabian Fernandez <fdzfabian@gmail.com>
2023-09-13 11:10:51 -0600

    Update welcome message to homepage

--- src/app/(with-menu)/(protected)/home/page.tsx
+++ src/app/(with-menu)/(protected)/home/page.tsx
@@ -1,7 +1,10 @@
+import { useStore } from "@/store";
+
 export default function Home() {
+  const { user } = useStore();
   return (
     <main className="w-full min-h-full text-left flex flex-col pt-5 font-semibold">
-      <h1 className="m-auto text-5xl">Hello!</h1>
+      <h1 className="m-auto text-5xl">Hello {user?.name}!</h1>
     </main>
   );
 }
